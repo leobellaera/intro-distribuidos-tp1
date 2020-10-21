1d31663c82e76bdd99e6b38cb65b1877d0a515a0
leo <lbellaera@fi.uba.ar>
2020-10-21 18:46:13 -0300

    Agrega esqueleto de sockets para servidor y cliente

--- client/main.py
+++ client/main.py
@@ -1,2 +1,33 @@
-if __name__ == '__main__':
-    print("Hello world")
+import argparse
+import socket
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-H", "--host", default="127.0.0.1")
+    parser.add_argument("-P", "--port", type=int, default="8080")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    server_address = (args.host, args.port)
+
+    # Create socket and connect to server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(server_address)
+    sock.send(b'c')
+
+    signal = sock.recv(1)
+    if signal.decode() != "s":
+        print("There was an error on the server")
+        exit(1)
+
+    print(signal.decode())
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
--- server/main.py
+++ server/main.py
@@ -1,2 +1,34 @@
-if __name__ == '__main__':
-    print("Hello world")
+import argparse
+import socket
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-H", "--host", default="127.0.0.1")
+    parser.add_argument("-P", "--port", type=int, default="8080")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    address = (args.host, args.port)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(address)
+    sock.listen(1)
+
+    while True:
+        conn, addr = sock.accept()
+        if not conn:
+            break
+
+        if conn.recv(1).decode() == 'c':
+            conn.send(b's')
+
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
